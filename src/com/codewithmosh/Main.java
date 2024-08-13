604d67d964903e9696b45e9a0900dcc6700a3d6e
omurilolima <muriloserafimlima@gmail.com>
2024-08-13 15:50:37 +0100

    Primitive types

---
+++ src/com/codewithmosh/Main.java
@@ -0,0 +1,15 @@
+package com.codewithmosh;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        String message = "Hello \"World\"" + " !!!";
+        System.out.println(message);
+        System.out.println(message.replace("!", "*"));
+        System.out.println(message);
+
+        String message2 = "c:\\Windows\\...";
+        System.out.println(message2);
+    }
+}
