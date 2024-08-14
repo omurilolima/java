9a28690aa28aa9d827e92fd5a0ae8623cca2763d
omurilolima <muriloserafimlima@gmail.com>
2024-08-14 16:03:45 +0100

    division

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        final float PI = 3.14F;  // use the prefix final to make a constant
+        int result1 = 10 / 3;
+        double result2 = (double)10 / (double)3;  // In java, a division of two integers is an integer. So we need to use double for precision
+        System.out.println(result1);
+        System.out.println(result2);
 
 
     }
